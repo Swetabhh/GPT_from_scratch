c3c196a5429692e16032f83f0a86b4eb6f0beb8b
Swetabh Sinha <swetabhsinha05@gmail.com>
2026-04-09 21:54:53 +0530

    Update bigram logic

--- bigram.py
+++ bigram.py
@@ -1,3 +1,4 @@
+from sympy import Idx
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -56,3 +57,61 @@ def estimate_loss():
 
 # Super simple bigram model
 class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size
+        
+    def forward(self, idx, target=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx) # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape 
+            logits = logits.view(B*T, c)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generater(self, idx, max_new_tokens):
+        # idx is (B,T) array of incides in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step 
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softnmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distributions
+            idx_next = torch.multimonial(probs, num_samples=1) # (B, 1) 
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return Idx
+
+model = BigramLanguageModel(vocab_size
+m = model.to(device)
+
+# create a PyTorch optimizer 
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss o train and val sets
+    if iter % eval_interval == 0;
+        loss = estimate_loss()
+        print(f"step (iter): train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    
+    # sample a batch of data
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model 
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
