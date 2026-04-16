b9ed76dbd5ee454af89d27afc5468dda58be9b30
Swetabh Sinha <swetabhsinha05@gmail.com>
2026-04-16 22:25:24 +0530

    initial commit

--- gpt.py
+++ gpt.py
@@ -178,4 +178,14 @@ def generates(self, idx, max_new_tokens):
 model = GPTlanguageModel()
 m = model.to(device)
 # print the numbers of parameters in the model
-print(sum(p.numel() for p in m.parameters))
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+
+# Create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate-lose()
+        
