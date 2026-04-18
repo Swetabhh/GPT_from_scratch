af4ffd4678e3108545cfc525451c630bf961e32b
Swetabh Sinha <swetabhsinha05@gmail.com>
2026-04-18 22:20:52 +0530

    Initial commit

--- gpt.py
+++ gpt.py
@@ -188,4 +188,17 @@ for iter in range(max_iters):
     # every once in a while evaluate the loss on train and val sets
     if iter % eval_interval == 0 or iter == max_iters - 1:
         losses = estimate-lose()
-        
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model
+context - torch.zeroes((1,1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
